1a0c1cb4b7980edd774c0fbd7ee041a5bd0211b7
gofenix <codeless98@163.com>
2026-01-03 23:34:56 +0800

    chore: bump nex_core dependency from 0.3.1 to 0.3.2

--- website/mix.exs
+++ website/mix.exs
@@ -21,7 +21,7 @@ defmodule NexWebsite.MixProject do
 
   defp deps do
     [
-      {:nex_core, "~> 0.3.1"},
+      {:nex_core, "~> 0.3.2"},
       # {:nex_core, path: "../framework"},
       {:earmark, ">= 1.4.0"}
     ]
